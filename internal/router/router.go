658661a9ee088bd09adecd5c2e6f32696b7bc7ec
vino <vino0908@outlook.com>
2023-04-07 23:50:20 +0800

    add not found router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,33 @@
 package router
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/vinoMamba/go-mangosteen/internal/controller"
 )
 
 func New() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+	//404 路由
+	r.NoRoute(func(c *gin.Context) {
+		acceptStr := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptStr, "text/html") {
+			c.String(http.StatusNotFound, "页面404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err_code":    1,
+				"err_message": "未定义的路由",
+			})
+		}
+
+	})
+
 	r.GET("/ping", controller.PingHandle)
 	return r
 }
