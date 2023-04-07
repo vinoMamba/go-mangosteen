cb2b33333435060604facb9e1a97f4a85f3b462f
vino <vino0908@outlook.com>
2023-04-07 23:03:15 +0800

    连接数据库

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,10 +3,14 @@ package cmd
 import (
 	"log"
 
+	"github.com/vinoMamba/go-mangosteen/internal/database"
 	"github.com/vinoMamba/go-mangosteen/internal/router"
 )
 
 func RunServer() {
+	database.PgConnect()
+	database.PgCreateTables()
+	defer database.Close()
 
 	r := router.New()
 	err := r.Run(":3000")
